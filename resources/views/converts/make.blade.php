73ec020b772e876fd85c209c9e0e669f0e9dd1da
Maxime Maillet <maxime.maillet93@gmail.com>
2019-06-07 19:26:26 +0200

    raw vs html

--- resources/views/converts/make.blade.php
+++ resources/views/converts/make.blade.php
@@ -11,12 +11,36 @@
     <div class="box box-solid box-success">
         <div class="box-header with-border">
             <h3 class="box-title">Results</h3>
+            <div class="box-tools pull-right">
+                <a id="change-html" class="label label-default">HTML</a>
+                <a id="change-raw" class="label label-default" style="display: none">RAW</a>
+            </div>
         </div>
-        <div class="box-body">
+        <div class="box-body" id="result-raw">
+            {!! $resultTemplate !!}
+        </div>
+        <div class="box-body" id="result-html" style="display: none;">
             {{ $resultTemplate }}
         </div>
     </div>
     @endif
 
     {!! form($form) !!}
+@stop
+
+@section('js')
+    <script>
+	    $("#change-html").click(function() {
+            $("#result-raw").css('display', 'none');
+            $("#change-html").css('display', 'none');
+            $("#change-raw").css('display', 'block');
+            $("#result-html").css('display', 'block');
+	    });
+	    $("#change-raw").click(function() {
+            $("#result-raw").css('display', 'block');
+            $("#change-html").css('display', 'block');
+            $("#change-raw").css('display', 'none');
+            $("#result-html").css('display', 'none');
+	    });
+    </script>
 @stop
